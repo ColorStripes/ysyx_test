8d4229d5430f00fcd0acbd286a77470b3bc5db27
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-25 18:07:58 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:07:58 up  6:54,  1 user,  load average: 0.10, 0.04, 0.01

--- npc/build/obj_dir/Vtop.mk
+++ npc/build/obj_dir/Vtop.mk
@@ -4,7 +4,7 @@
 # Execute this makefile from the object directory:
 #    make -f Vtop.mk
 
-default: Vtop__ALL.a
+default: /home/xu/ysyx-workbench/npc/build/top
 
 ### Constants...
 # Perl executable (from $PERL)
@@ -35,17 +35,24 @@ VM_PREFIX = Vtop
 VM_MODPREFIX = Vtop
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
+	-I/home/xu/ysyx-workbench/nvboard/include \
+	-DTOP_NAME="Vtop" \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
+	/home/xu/ysyx-workbench/nvboard/build/nvboard.a \
+	-lSDL2 \
+	-lSDL2_image \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
-	a \
+	auto_bind \
+	na \
 
 # User .cpp directories (from .cpp's on Verilator command line)
 VM_USER_DIR = \
-	csrc \
+	/home/xu/ysyx-workbench/npc/build \
+	/home/xu/ysyx-workbench/npc/csrc \
 
 
 ### Default rules...
@@ -53,5 +60,18 @@ VM_USER_DIR = \
 include Vtop_classes.mk
 # Include global rules
 include $(VERILATOR_ROOT)/include/verilated.mk
+
+### Executable rules... (from --exe)
+VPATH += $(VM_USER_DIR)
+
+auto_bind.o: /home/xu/ysyx-workbench/npc/build/auto_bind.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+na.o: /home/xu/ysyx-workbench/npc/csrc/na.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+
+### Link rules... (from --exe)
+/home/xu/ysyx-workbench/npc/build/top: $(VK_USER_OBJS) $(VK_GLOBAL_OBJS) $(VM_PREFIX)__ALL.a $(VM_HIER_LIBS)
+	$(LINK) $(LDFLAGS) $^ $(LOADLIBES) $(LDLIBS) $(LIBS) $(SC_LIBS) -o $@
+
 
 # Verilated -*- Makefile -*-
