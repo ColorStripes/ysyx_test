73a33849d7a041bfe9926e9dc4fd1eec4683d5bf
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-27 21:36:21 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:36:21 up 1 day,  5:38,  1 user,  load average: 0.10, 0.03, 0.01

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -90,7 +90,7 @@ static int cmd_x(char *args){
       sscanf(add,"%lx",&addr);
       //printf("%lx",vaddr_read(addr, 3));
     }
-    vaddr_read(addr, 3);
+    printf("%lx",vaddr_read(addr, 3));
     return 0;
 }
 
