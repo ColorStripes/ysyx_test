4ce2c2cb41fcdabbe75075b23b4c2fb68648163f
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-27 19:15:06 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:15:06 up 1 day,  3:17,  1 user,  load average: 0.10, 0.09, 0.03

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -9,8 +9,11 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-    for(int i = 0;i < 32; i++)
-        printf("%s = %d\n",regs[i],*regs[i]);
+    for(int i = 0;i < 32; i++){
+        printf("%s = %d   ",regs[i],*regs[i]);
+        if(i%8 == 0)
+            printf("\n");
+    }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
