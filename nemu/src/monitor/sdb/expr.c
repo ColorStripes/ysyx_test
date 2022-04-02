2b7f904b8421a1bb16de4d5bcd030c4c37f558eb
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 02:45:43 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:45:43 up  2:55,  1 user,  load average: 0.04, 0.04, 0.07

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -226,7 +226,7 @@ uint32_t eval(int p,int q) {
   else {
     /* We should do more things here. */
       int op = Primary_op(p,q);                  // the position of 主运算符 in the token expression;
-      printf("%d,%c\n",op,tokens[op].type);
+      printf("%d,%c,%d,%d\n",op,tokens[op].type,p,q);
       uint32_t val1 = eval(p, op - 1);
       uint32_t val2 = eval(op + 1, q);
 
