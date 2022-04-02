df1f265724e98e84354b55ffec521d7fc5018f25
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-02 23:32:33 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:32:33 up  5:29,  1 user,  load average: 0.03, 0.03, 0.03

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -17,6 +17,7 @@ int main(int argc, char *argv[]) {
   uint32_t result;
   char exp[33]={};
   FILE *fp = fopen("home/xu/ysyx-workbench/nemu/tools/gen-expr/build/input","r");
+  if(fp == NULL)
   assert(fp);
   for(int i = 0; i < 1; i++){
   if(fscanf(fp,"%d %s",&result,exp));
