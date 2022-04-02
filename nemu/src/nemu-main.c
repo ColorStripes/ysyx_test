69207ed602f721267053b8c8e861b81eac832f8c
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-02 23:36:09 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:36:09 up  5:32,  1 user,  load average: 0.05, 0.07, 0.04

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
   if(fp == NULL)
   assert(fp);
   for(int i = 0; i < 1; i++){
-  if(fscanf(fp,"%d %s",&result,exp));
+  if(fscanf(fp,"%d",&result));
   printf("%d,%s\n",result,exp);
   }
   fclose(fp);
