08a2683887fd7ca2f0a0a073f1b02dc1d7bca820
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 03:27:13 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:27:13 up  3:36,  1 user,  load average: 0.22, 0.13, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -262,7 +262,7 @@ word_t expr(char *e, bool *success) {
      int t = 0;
      for(int i = 0; i < 32;i++)
      if(tokens[i].type != 0)  t++;
-     eval(0,t-1);
+     return eval(0,t-1);
 
   return 0;
 }
