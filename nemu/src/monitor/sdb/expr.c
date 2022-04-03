9665ab10c6989a4197952726c1c0abf0e0bed115
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 15:53:01 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:53:01 up  6:41,  1 user,  load average: 0.12, 0.05, 0.07

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -152,7 +152,7 @@ bool check_parentheses(int p , int q){
 		else if (cnt != 0 && (tmp[cnt - 1] + 1 == tokens[i].type || tmp[cnt - 1] + 2 == tokens[i].type)){
 		//printf("wc :%d\n",q);
 			if(cnt == 1){
-			  if(i != q)
+			  //if(i != q)
 			       printf("wc :%d ,%d\n",i,q);
 				//NW_paren = 1;
 			cnt--;
