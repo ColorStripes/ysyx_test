2df1f1762505a85db4cfa2c177b02f86bdf5a457
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 16:20:55 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:20:55 up  7:09,  1 user,  load average: 0.00, 0.03, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -232,7 +232,7 @@ uint32_t eval(int p,int q) {
     /* The expression is surrounded by a matched pair of parentheses.
      * If that is the case, just throw away the parentheses.
      */
-     printf("true:%d %d\n",p,q);
+     //printf("true:%d %d\n",p,q);
     return eval(p + 1, q - 1);
   }
   else {
