b15b5ba7991dba66217863fde01242b9d366dab4
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 16:23:04 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:23:04 up  7:11,  1 user,  load average: 0.17, 0.07, 0.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -209,7 +209,7 @@ int Primary_op(int p , int q){
 uint32_t eval(int p,int q) {
   if (p > q) {
     printf("p is %d > q is %d\n",p,q);
-    assert("This is a bad expression. Stop!\n");
+    assert(0);
     return 0;    
   }
   else if (p == q) {
