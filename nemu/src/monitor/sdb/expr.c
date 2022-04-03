fda4e229afed831cc3f9bb662a28dfea888af23b
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 17:00:46 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:00:46 up  7:49,  1 user,  load average: 0.05, 0.06, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -246,7 +246,7 @@ uint32_t eval(int p,int q) {
       uint32_t val2 = eval(op + 1, q);
       
       
-      printf("val1:%d,val2:%d\n",val1,val2);
+      printf("val1:%d,op:%c,val2:%d\n",val1,op,val2);
 
     switch (tokens[op].type) {
       case '+': return val1 + val2;
