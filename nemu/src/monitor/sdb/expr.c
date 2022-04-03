21125b478cc6b615b080294112ff6062833649b1
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 19:22:33 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:22:33 up 10:10,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -253,18 +253,18 @@ uint32_t eval(int p,int q) {
       //printf("val1:%d, op:%c, val2:%d   ops:%d\n",val1,tokens[op].type,val2,op);
 
     switch (tokens[op].type) {
-      case '+': printf("val1:%d, op:%c, val2:%d   ops:%d    res:%u\n",val1,tokens[op].type,val2,op,val1+val2);
+      case '+': printf("val1:%d, op:%c, val2:%d   ops:%d    res:%u   p:%d,  q:%d\n",val1,tokens[op].type,val2,op,val1+val2,p,q);
       return val1 + val2;
-      case '-': printf("val1:%d, op:%c, val2:%d   ops:%d    res:%u\n",val1,tokens[op].type,val2,op,val1-val2);
+      case '-': printf("val1:%d, op:%c, val2:%d   ops:%d    res:%u   p:%d,  q:%d\n",val1,tokens[op].type,val2,op,val1-val2,p,q);
       return val1 - val2;
-      case '*': printf("val1:%d, op:%c, val2:%d   ops:%d    res:%u\n",val1,tokens[op].type,val2,op,val1*val2);
+      case '*': printf("val1:%d, op:%c, val2:%d   ops:%d    res:%u   p:%d,  q:%d\n",val1,tokens[op].type,val2,op,val1*val2,p,q);
       return val1 * val2;
       case '/': if(val2 == 0){
 			printf("####This is division by 0 expression.#####\n");
 		        return 0;
 		}
 		else{
-		        printf("val1:%d, op:%c, val2:%d   ops:%d    res:%u\n",val1,tokens[op].type,val2,op,val1/val2);
+		        printf("val1:%d, op:%c, val2:%d   ops:%d    res:%u  p:%d,  q:%d\n",val1,tokens[op].type,val2,op,val1/val2,p,q);
 		        return val1 / val2;
 		        }
       default: assert(0);
