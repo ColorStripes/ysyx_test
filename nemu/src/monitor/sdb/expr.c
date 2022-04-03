3dd03cf188db0850a9c2eafc696aa763362f71d5
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 15:58:25 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:58:25 up  6:46,  1 user,  load average: 0.01, 0.04, 0.07

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -162,7 +162,7 @@ bool check_parentheses(int p , int q){
 		assert("The expression is illegal.\n");
 
 	if(NW_paren){
-                printf("true:%d %d\n",p,q);
+                printf("flase:%d %d\n",p,q);
 		return false;
 		}
 	else{
