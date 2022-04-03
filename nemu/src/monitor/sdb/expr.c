c7859a337b6a5786e7de8c812b5b4c9d8b849073
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 19:04:22 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:04:22 up  9:51,  1 user,  load average: 0.07, 0.07, 0.03

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -242,12 +242,12 @@ uint32_t eval(int p,int q) {
       int op = Primary_op(p,q);                  // the position of 主运算符 in the token expression;
       //printf("%d,%c,%d,%d\n",op,tokens[op].type,p,q);
       //assert(0);
-      uint32_t val1 = eval(p, op - 1);
-      uint32_t val2 = eval(op + 1, q);
-      unsigned int cc = 13;
-      unsigned int dd = -2;
-      uint32_t b= cc / dd;
-      printf("www:%u %u %u\n",cc,dd,b);
+      int val1 = eval(p, op - 1);
+      int val2 = eval(op + 1, q);
+      //unsigned int cc = 13;
+      //unsigned int dd = -2;
+      //uint32_t b= cc / dd;
+      //printf("www:%u %u %u\n",cc,dd,b);
       //uint32_t b= val1 / val2;
       //printf("www:%u %u %u\n",val1,val2,b);
       //printf("val1:%d, op:%c, val2:%d   ops:%d\n",val1,tokens[op].type,val2,op);
