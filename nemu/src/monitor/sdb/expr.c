26d7508b225c46fff8b69715ab0ae5525afbbc1c
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-23 23:29:27 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:29:27 up 12:37,  1 user,  load average: 0.17, 0.06, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -370,10 +370,16 @@ word_t expr(char *e, bool *success) {
      
      
   for (int i = 0; i < nr_token; i ++) {
-  	if (tokens[i].type == '*' && (i == 0 || tokens[i - 1].type == '(') ) {
+  	if (tokens[i].type == '*' && 
+  	   (i == 0 || tokens[i - 1].type == '(' || 
+  	   (tokens[i - 1].type != ')' && tokens[i - 1].type != TK_NUMBER && tokens[i - 1].type != TK_HEXNUM && tokens[i - 1].type != TK_REG)))
+        {
    	      tokens[i].type = DEREF;
   	}
-  	else if (tokens[i].type == '-' && (i == 0 || tokens[i - 1].type == '(') ) {
+  	else if (tokens[i].type == '-' && 
+  	   (i == 0 || tokens[i - 1].type == '(' || 
+  	   (tokens[i - 1].type != ')' && tokens[i - 1].type != TK_NUMBER && tokens[i - 1].type != TK_HEXNUM && tokens[i - 1].type != TK_REG)))
+  	{
    	      tokens[i].type = NEGAT;
   	}
   }
