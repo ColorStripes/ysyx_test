b221c933f55996c40f8e6ba8faeeb3fc05fce2d2
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-24 02:24:07 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:24:07 up 15:31,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -357,7 +357,7 @@ uint64_t eval(int p,int q) {
 
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
-    *success = false;
+    *success = false;printf("*****%d ***\n",*success);  
     return 0;
   }
   //printf("9 is %s\n",tokens[9].str);
@@ -368,7 +368,7 @@ word_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
      //printf("long:%d\n",nr_token);
      
-  printf("*****%d ***\n",*success);  
+  
   for (int i = 0; i < nr_token; i ++) {
   	if (tokens[i].type == '*' && 
   	   (i == 0 || tokens[i - 1].type == '(' || 
