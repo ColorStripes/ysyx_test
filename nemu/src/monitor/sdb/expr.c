2e6f12d208ccc334e59a7050ffb3f4b12000b7d4
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-23 22:29:17 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:29:17 up 11:37,  1 user,  load average: 0.08, 0.06, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -235,7 +235,7 @@ int Primary_op(int p , int q){
                 }
                 else if((tokens[i].type == '+' || tokens[i].type == '-' || tokens[i].type == '*' || tokens[i].type == '/')&&
                         (op = -1 || tokens[op].type == '+' || tokens[op].type == '-' || tokens[op].type == '*' || tokens[op].type == '/'  ))
-                {
+                {printf("%d,%d\n",i,tokens[i].type);
                         if(tokens[i].type == '+' || tokens[i].type == '-')
                                 op = i;
                         else{
@@ -248,10 +248,10 @@ int Primary_op(int p , int q){
                 else{
                         continue;
                 }
-                printf("%d,%d\n",i,tokens[i].type);
+               // printf("%d,%d\n",i,tokens[i].type);
         }
         
-        printf("%d,,\n",op);
+        //printf("%d,,\n",op);
         if(op == -1)
                 printf("no main option.\n");
                 //assert(0);
