8804537b5e960987687bd26699a9759c4bf6e414
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-23 22:42:28 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:42:28 up 11:50,  1 user,  load average: 0.10, 0.04, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -235,12 +235,12 @@ int Primary_op(int p , int q){
                 }
                 else if((tokens[i].type == '+' || tokens[i].type == '-' || tokens[i].type == '*' || tokens[i].type == '/')&&
                         (op = -1 || tokens[op].type == '+' || tokens[op].type == '-' || tokens[op].type == '*' || tokens[op].type == '/'  ))
-                {//printf("%d,%d\n",i,tokens[i].type);
+                {printf("%d,%d,%d\n",i,op,tokens[op].type);
                         if(tokens[i].type == '+' || tokens[i].type == '-'){
                                 op = i;
                                 //printf("**%d,%d,%d\n",i,op,tokens[op].type);
                         }
-                        else{printf("%d,%d,%d\n",i,op,tokens[op].type);
+                        else{//printf("%d,%d,%d\n",i,op,tokens[op].type);
                                 if(op == -1)
                                         op = i;
 				else if(tokens[op].type == '*' || tokens[op].type == '/')
