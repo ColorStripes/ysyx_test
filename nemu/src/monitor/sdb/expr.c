71a93c21a02c094d1d3e2e1f9587b9b54bc8beb1
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-23 22:43:36 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:43:36 up 11:51,  1 user,  load average: 0.07, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -235,7 +235,7 @@ int Primary_op(int p , int q){
                 }
                 else if((tokens[i].type == '+' || tokens[i].type == '-' || tokens[i].type == '*' || tokens[i].type == '/')&&
                         (op = -1 || tokens[op].type == '+' || tokens[op].type == '-' || tokens[op].type == '*' || tokens[op].type == '/'  ))
-                {printf("%d,%d,%d\n",i,op,tokens[op].type);
+                {printf("*%d,%d,%d\n",i,op,tokens[op].type);
                         if(tokens[i].type == '+' || tokens[i].type == '-'){
                                 op = i;
                                 //printf("**%d,%d,%d\n",i,op,tokens[op].type);
@@ -247,7 +247,7 @@ int Primary_op(int p , int q){
 					op = i;
 			        //printf("*%d,%d,%d\n",i,op,tokens[op].type);
                         }
-                        //printf("%d,%d,%d\n",i,op,tokens[op].type);
+                        printf("**%d,%d,%d\n",i,op,tokens[op].type);
                 }
                 else{
                         continue;
