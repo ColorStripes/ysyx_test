242ff3109e43843ce2cd83b2dec77b232e6bbc06
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 17:23:49 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:23:49 up 1 day, 23:59,  1 user,  load average: 0.24, 0.12, 0.10

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -29,7 +29,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   
-#ifdef CONFIG_WATCHPOINT
+//#ifdef CONFIG_WATCHPOINT
   bool isdebug=false;
   scan_wp(&isdebug);
   if(isdebug){
@@ -37,7 +37,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
      printf("Debug on there.\n");
      sdb_mainloop();
   }
-#endif
+//#endif
 
 }
 
