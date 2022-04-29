2391b960d0fff45428fc09cea069701fc649fc14
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 13:13:30 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:13:30 up 1 day, 19:48,  1 user,  load average: 0.02, 0.12, 0.08

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -54,7 +54,7 @@ static long load_img() {
 }
 
 static int parse_args(int argc, char *argv[]) {
-  printf("******%s****\n",*argv);
+  printf("******%d****\n",argc);
   const struct option table[] = {
     {"batch"    , no_argument      , NULL, 'b'},
     {"log"      , required_argument, NULL, 'l'},
