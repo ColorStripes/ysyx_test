6b20ac6579c279afbb41ad86bafaf13f3f79c38d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-30 12:43:53 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:43:53 up 2 days,  3:59,  1 user,  load average: 0.33, 0.19, 0.07

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,149 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  assert(NULL != s);
+  size_t count = 0;
+  const char *temp = s;
+  while(*temp != '\0'){
+  	count ++;
+  	temp ++;
+  }
+  return count;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  assert(NULL != dst);
+  assert(NULL != src);
+  if(dst == NULL || src == NULL){
+      return NULL;
+  }
+  char *ret = dst;
+  while((*dst++ = *src++) != '\0');
+  return ret;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  assert(NULL != dst);
+  assert(NULL != src);
+  if(dst == NULL || src == NULL){
+      return NULL;
+  }
+  if (n < 0) return NULL;
+  char *ret = dst;
+  while(n-- && (*dst++ = *src++));
+  while(n--) *dst++ = '\0'; 
+  return ret;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  assert(NULL != dst);
+  assert(NULL != src);
+  if(dst == NULL || src == NULL){
+      return NULL;
+  }
+  char *ret = dst;
+  while(*dst++ != '\0');
+  while((*dst++ = *src++) != '\0') ;
+  return ret;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  assert(NULL != s1);
+  assert(NULL != s2);
+  if(s1 == NULL || s2 == NULL) return 0;
+  
+  int ret = 0;
+  while (!(ret = *s1 - *s2) && *s2)
+  {
+	s1++;
+	s2++;
+  }
+
+  if (ret < 0)
+	ret = -1;
+  else if (ret > 0)
+	ret = 1;
+  return ret;
+  
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  assert(NULL != s1);
+  assert(NULL != s2);
+  if(s1 == NULL || s2 == NULL) return 0;
+  if (n < 0) return 0;
+  
+  int ret = 0;
+  while(n-- && !(ret = *s1 - *s2) && *s2){
+       s1++;
+       s2++;
+  }
+  if (ret < 0)
+	ret = -1;
+  else if (ret > 0)
+	ret = 1;
+  return ret;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  if (s == NULL || n < 0)
+  {
+	return NULL;
+  }
+  char *pdest = (char *)s;
+  while (n-- > 0)
+  {
+	*pdest++ = c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  if (n == 0) return NULL;
+  if (dst == NULL || src == NULL)  return NULL;
+ 
+  char* dst_ = (char*)dst;
+  char* src_ = (char*)src;
+  if ((dst_ > src_) && (dst_ < src_ + n))//判断内存重叠时的情况反向拷贝（如果正向，会改变模板原来的值）
+  {
+	printf("发生重叠\n");
+	while (n--)
+	    *(dst_ + n) = *(src_ + n);
+  }
+  else //不重叠情况正向拷贝（如果反向，也会改变模板原来的值）
+  {
+	while(n--)
+	    *dst_++ = *src_++;
+  }
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  if (n == 0) return NULL;
+  if (out == NULL || in == NULL)  return NULL;
+  char* out_ = (char*)out;
+  char* in_ = (char*)in;
+  while(n--) *out_++ = *in_++;
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+   assert(s1);
+   assert(s2);
+   if (s1 == NULL || s2 == NULL)  return 0;
+   if (n == 0) return 0;
+   int ret = 0;
+   char* s1_ = (char*)s1;
+   char* s2_ = (char*)s2;
+   while(n-- && !(ret = *s1_ - *s2_)){
+        s1_++;
+        s2_++;
+   }
+   if (ret < 0)
+	 ret = -1;
+   else if (ret > 0)
+	 ret = 1;
+   return ret;
 }
 
 #endif
