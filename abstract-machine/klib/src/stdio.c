d84bf803210e08bbf9b178583b6cc95692c1d885
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-01 23:34:31 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:34:31 up 2 days, 11:32,  1 user,  load average: 0.12, 0.05, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,26 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+void itoa(unsigned int n, char * buf)
+{
+        int i;
+        
+        if(n < 10)
+        {
+                buf[0] = n + '0';
+                buf[1] = '\0';
+                return;
+        }
+        itoa(n / 10, buf);
+
+        for(i=0; buf[i]!='\0'; i++);
+        
+        buf[i] = (n % 10) + '0';
+        
+        buf[i+1] = '\0';
+}
+
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +34,50 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  
+  int count = 0;
+  char buf[65];
+  int d;
+  char c;
+  memset(buf, 0, sizeof(buf));
+  va_list ap;
+  va_start(ap, fmt);
+  while(*fmt){
+    if(*fmt == '%'){
+        fmt++;
+  	switch(*fmt){
+  	    case 's':
+  	    	out = va_arg(ap, char *);
+  	    	break;
+  	    case 'd':
+  	        d = va_arg(ap, int);
+  	        if(d < 0){
+  	            *out = '-';
+  	            out ++;
+  	            d = -d;
+  	            count++;
+  	        }
+  	        itoa(d, buf);
+  	        memcpy(out, buf, strlen(buf));
+  	        out += strlen(buf);
+  	        count += strlen(buf);
+  	        break;
+  	    case 'c':
+  	        c = (char)va_arg(ap, int);
+  	        *out = c;
+  	        out ++;
+  	        count++;
+  	        break;
+  	}
+    }
+    else{
+  	 *out++ = *fmt++;
+  	 count++;
+    }  
+  }	    	
+  va_end(ap);
+  return count;
+
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
