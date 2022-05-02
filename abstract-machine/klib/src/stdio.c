33053a30ba0e7d9a915dc8cd170e0a9218cfec0b
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-02 17:02:41 +0800

    > compile NEMU ysyx_22040931 徐鑫 Linux xu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:02:41 up 2 days, 14:51,  1 user,  load average: 0.11, 0.07, 0.02

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -23,6 +23,9 @@ void itoa(unsigned int n, char * buf)
         
         buf[i+1] = '\0';
 }
+
+
+//conversions
 void xtoa(unsigned int n, char * buf)
 {
         int i;
@@ -55,6 +58,61 @@ void xtoa(unsigned int n, char * buf)
         buf[i + 1] = '\0';
 }
 
+void Xtoa(unsigned int n, char * buf)
+{
+        int i;
+        
+        if(n < 16)
+        {
+                if(n < 10)
+                {
+                        buf[0] = n + '0';
+                }
+                else
+                {
+                        buf[0] = n - 10 + 'A';
+                }
+                buf[1] = '\0';
+                return;
+        }
+        xtoa(n / 16, buf);
+        
+        for(i = 0; buf[i] != '\0'; i++);
+        
+        if((n % 16) < 10)
+        {
+                buf[i] = (n % 16) + '0';
+        }
+        else
+        {
+                buf[i] = (n % 16) - 10 + 'A';
+        }
+        buf[i + 1] = '\0';
+}
+
+void otoa(unsigned int n, char * buf)
+{
+        int i;
+        if(n < 8)
+        {
+        	buf[0] = n + '0';
+                buf[1] = '\0';
+                return;
+        }
+        xtoa(n / 8, buf);
+        for(i = 0; buf[i] != '\0'; i++);
+        buf[i] = (n % 8) + '0';
+        buf[i + 1] = '\0';
+}
+
+
+int isnum(unsigned char c)
+{
+    if (c >= '0' && c <= '9')
+        return 1;
+    else
+        return 0;
+}
 
 int printf(const char *fmt, ...) {
   panic("Not implemented");
@@ -72,6 +130,10 @@ int sprintf(char *out, const char *fmt, ...) {
   char *s;
   int d;
   char c;
+  int len;
+  int num;
+  int numindex = 0;
+  char Num[16] = {'0'};
   memset(buf, 0, sizeof(buf));
   va_list ap;
   va_start(ap, fmt);
@@ -87,11 +149,11 @@ int sprintf(char *out, const char *fmt, ...) {
   	    case 'd':
   	        d = va_arg(ap, int);
   	        if(d < 0){
-  	            *out = '-';
-  	            out ++;
-  	            d = -d;
-  	        }
-  	        itoa(d, buf);
+                    *out = '-';
+                    out ++;
+                    d = -d;
+                }
+                itoa(d, buf);
   	        memcpy(out, buf, strlen(buf));
   	        out += strlen(buf);
   	        break;
@@ -101,11 +163,119 @@ int sprintf(char *out, const char *fmt, ...) {
   	        out ++;
   	        break;
   	    case 'x': /*0x*/
-                d = va_arg(ap, int);
+  	        d = va_arg(ap, int);
                 xtoa(d, buf);
                 memcpy(out, buf, strlen(buf));
                 out += strlen(buf);
                 break;
+  	    case 'X':
+                d = va_arg(ap, int);
+                Xtoa(d, buf);
+                memcpy(out, buf, strlen(buf));
+                out += strlen(buf);
+                break;
+            case 'o':
+                d = va_arg(ap, int);
+                otoa(d, buf);
+                memcpy(out, buf, strlen(buf));
+                out += strlen(buf);
+                break;
+            case 'f':
+                break;
+            case '%': /*输出%*/
+                    *out = '%';
+                    out++;                    
+                    break;
+            case '0':
+                    if(*fmt != '-'){
+                        numindex = 0;
+                        while(isnum(*fmt)){
+                            Num[numindex++] = *fmt++;
+                        }
+                        Num[numindex++] = '\n';
+                        num = atoi(Num);
+                    }
+                    else{
+                        fmt++;
+                        break;
+                    }
+                    switch(*fmt++){
+                        case 'd':
+                            d = va_arg(ap, int);
+  	                    if(d < 0){
+                                *out = '-';
+                                out ++;
+                                d = -d;
+                            }
+                            itoa(d, buf);
+                            len = strlen(buf);
+                            if(len >= num){
+                               memcpy(out, buf, len);
+                               out += strlen(buf);  
+                            }
+                            else{
+                               memset(out, '0', num - len);
+                               out += num - len;
+                               memcpy(out, buf, len);
+                               out += len;
+                            }
+                            break;
+                         case 'o':
+                            d = va_arg(ap, int);
+                            otoa(d, buf);
+                            len = strlen(buf);
+                            if(len >= num){
+                               memcpy(out, buf, len);
+                               out += len;  
+                            }
+                            else{
+                               memset(out, '0', num - len);
+                               out += num - len;
+                               memcpy(out, buf, len);
+                               out += len;
+                            }
+                            break;
+                         
+                    }
+                    break;
+            case '-':
+                    numindex = 0;
+                    while(isnum(*fmt)){
+                        Num[numindex++] = *fmt++;
+                    }
+                    Num[numindex++] = '\n';
+                    num = atoi(Num);
+                    break;
+            case '#':
+                    switch(*fmt++){
+                        case 'o':
+                             d = va_arg(ap, int);
+                             otoa(d, buf);
+                             if(*buf != '0'){
+                                *out = '0';
+                                out ++;
+                             }
+                             memcpy(out, buf, strlen(buf));
+                             out += strlen(buf);
+                             break;
+                         case 'x':
+                             *out++ = '0';
+                             *out++ = 'x'; 
+                             d = va_arg(ap, int);
+                             xtoa(d, buf);
+                             memcpy(out, buf, strlen(buf));
+                             out += strlen(buf);
+                             break;
+                          case 'X':
+                             *out++ = '0';
+                             *out++ = 'X'; 
+                             d = va_arg(ap, int);
+                             Xtoa(d, buf);
+                             memcpy(out, buf, strlen(buf));
+                             out += strlen(buf);
+                             break;
+                             
+                    }
   	    default:
   	        return -1;
   	        
